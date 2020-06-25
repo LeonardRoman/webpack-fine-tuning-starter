480176bbef02d178a77fd65886f2208432a0c01b
LeonardRoman <leonardroman1@gmail.com>
2020-06-25 12:31:48 +0400

    Урок 4.1: es6 import for constants.js

--- scripts/webpack/constants.js
+++ scripts/webpack/constants.js
@@ -1,11 +1,11 @@
 // Core
 import { resolve } from 'path'
-import { path } from 'app-root-path'
+import { path as PROJECT_ROOT } from 'app-root-path'
 
 // the path(s) that should be cleaned
-exports.PROJECT_ROOT = path
-exports.BUILD_DIRECTORY = resolve(path, './build')
-exports.SOURCE_DIRECTORY = resolve(path, './src')
-exports.PUBLIC_DIRECTORY = resolve(path, './static')
-exports.HOST = 'localhost'
-exports.PORT = 3000
+export { PROJECT_ROOT }
+export const BUILD_DIRECTORY = resolve(PROJECT_ROOT, './build')
+export const SOURCE_DIRECTORY = resolve(PROJECT_ROOT, './src')
+export const PUBLIC_DIRECTORY = resolve(PROJECT_ROOT, './static')
+export const HOST = 'localhost'
+export const PORT = 3000
