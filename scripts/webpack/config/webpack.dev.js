95606b7c3d56ac8756baf162b454e85278fca375
LeonardRoman <leonardroman1@gmail.com>
2020-06-25 14:43:59 +0400

    Урок 4.2: добавлены source-map в dev

--- scripts/webpack/config/webpack.dev.js
+++ scripts/webpack/config/webpack.dev.js
@@ -8,7 +8,7 @@ import getCommonConfig from './webpack.common'
 module.exports = () => {
   return merge(getCommonConfig(), {
     mode: 'none',
-    devtool: false, // TODO: настроить source map
+    devtool: 'cheap-module-eval-source-map',
     entry: ['webpack-hot-middleware/client?reload=true&quiet=true'],
     plugins: [
       new HotModuleReplacementPlugin()
